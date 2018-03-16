792d2d43286e84664a3e8dfcd0a1ac15b897741c
Alexandre.Gerardi <alexandre.gerardi@cpnv.ch>
2018-03-16 15:04:49 +0100

    ajout mld au dossier

--- Site PIKA/homepage.php
+++ Site PIKA/homepage.php
@@ -12,8 +12,8 @@ ConnectDB();
         <meta name="PIKA" content="PIKA website">
         <link rel="shortcut icon" href="imgs/logo.png">
         <link rel="stylesheet" href="main.css">
-        <link rel="stylesheet" href="/css/bootstrap.css">
-        <script src="/js/bootstrap.js"></script>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <script src="js/bootstrap.js"></script>
         <script type="text/javascript">
             var adminclick = 0;
 
@@ -44,83 +44,9 @@ ConnectDB();
         <span class="containerlogo">
             <img class="logo" src="imgs/logo.png" onclick="add()">
         </span>
-        <table id="storetable">
-            <tr>
-                <td>
-                    <table>
-                        <tbody id="leftmenu">
-                            <tr>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="product">
-                                    placeholder
-                                </td>
-                            </tr>
-                    </table>
-                </td>
-                <td>
-                    <table id="rightmenu">
-                        <tr>
-                            <td>
-                                <h3>Panier</h3>
-                                <table id="basket">
-                                    <tr>
-                                        <td>
-                                            placeholder
-                                        </td>
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
-                        <tr class="rightmenbot">
-                            <td>
-                                <input type="submit" value="Commander" class="buttonright" id="buttonbig">
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-        </table>
+        <div class="container container-fluid">
+            <div class="alert alert-danger col-md-8">test</div>
+            <div class="alert alert-danger col-md-4">test2</div>
+        </div>
     </body>
 </html>
